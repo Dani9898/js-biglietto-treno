1317984998dabf6b286ab592fc11f39914adab31
Dani9898 <danieleoliva98@gmail.com>
2021-10-21 13:35:26 +0200

    inizializzazione esercizio

---
+++ js/script.js
@@ -0,0 +1,11 @@
+let elemento = document.getElementById("My-id");
+
+// chiediamo all'utente il numero di km che vuole percorrere e l'età del passeggero. In base a ciò calcolare il prezzo totale del viaggio secondo alcune regole: il prezzo del biglietto è definito in base ai km (0.21 € al km), sconto del 20% per i < 18 anni, sconto del 40% per i > 65 anni. La stampa del prezzo finale va fuori in forma umana (con massimo 2 decimali per indicare i centesimi)
+
+// chiediamo all'utente il n di km
+
+// chiediamo all'utente la sua età
+
+// calcoliamo il prezzo del biglietto
+
+// stampiamo il prezzo del biglietto
