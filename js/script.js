b120d971042335ff2f54eaabad5e14b7bba13c78
Dani9898 <danieleoliva98@gmail.com>
2021-10-21 15:02:17 +0200

    codice semplificato

--- js/script.js
+++ js/script.js
@@ -3,23 +3,19 @@ let elemento = document.getElementById("my-id");
 // chiediamo all'utente il numero di km che vuole percorrere e l'età del passeggero. In base a ciò calcolare il prezzo totale del viaggio secondo alcune regole: il prezzo del biglietto è definito in base ai km (0.21 € al km), sconto del 20% per i < 18 anni, sconto del 40% per i > 65 anni. La stampa del prezzo finale va fuori in forma umana (con massimo 2 decimali per indicare i centesimi)
 
 // chiediamo all'utente il n di km
-let numeroKmcorretto;
 let nKm = parseInt(prompt("Ciao, inserisci il numero di chilometri che vuoi fare:"));
 
 if (isNaN(nKm) == true) {
-    numeroKmcorretto = parseInt(prompt("Per favore inserisci un valore numerico corretto"));
-    nKm = numeroKmcorretto;
+    nKm = parseInt(prompt("Per favore inserisci un valore numerico corretto"));
 }
 
 console.log(nKm);
 
 // chiediamo all'utente la sua età
-let etàCorretta;
 let età = parseInt(prompt("Quanti anni hai?"));
 
 if (isNaN(età) == true) {
-    etàCorretta = parseInt(prompt("Per favore inserisci un valore numerico corretto"));
-    età = etàCorretta;
+    età = parseInt(prompt("Per favore inserisci un valore numerico corretto"));
 }
 
 console.log(età);
