463dcc104de12b248968eaa01f6db3909ce142d7
Dani9898 <danieleoliva98@gmail.com>
2021-10-21 14:33:10 +0200

    esercizio finito

--- js/script.js
+++ js/script.js
@@ -1,11 +1,25 @@
-let elemento = document.getElementById("My-id");
+let elemento = document.getElementById("my-id");
 
 // chiediamo all'utente il numero di km che vuole percorrere e l'età del passeggero. In base a ciò calcolare il prezzo totale del viaggio secondo alcune regole: il prezzo del biglietto è definito in base ai km (0.21 € al km), sconto del 20% per i < 18 anni, sconto del 40% per i > 65 anni. La stampa del prezzo finale va fuori in forma umana (con massimo 2 decimali per indicare i centesimi)
 
 // chiediamo all'utente il n di km
+let nKm = parseInt(prompt("Ciao, inserisci il numero di chilometri che vuoi fare:"));
+console.log(nKm);
 
 // chiediamo all'utente la sua età
+let età = parseInt(prompt("Quanti anni hai?"));
+console.log(età);
 
 // calcoliamo il prezzo del biglietto
+const prezzo1Km = 0.21;
+let prezzoBiglietto = nKm * prezzo1Km;
+
+if (età < 18) {
+    prezzoBiglietto = prezzoBiglietto * 0.8;
+} else if (età > 65) {
+    prezzoBiglietto = prezzoBiglietto * 0.6;
+}
+console.log(prezzoBiglietto);
 
 // stampiamo il prezzo del biglietto
+elemento.innerHTML = `Il costo del biglietto è di: ${prezzoBiglietto.toFixed(2)} €.`;
